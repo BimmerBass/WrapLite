b28dd143da51027db8a22e65a87abb599eef41fc
BimmerBass <abildskovniels@gmail.com>
2021-12-27 16:13:57 +0100

    Add methods to extract integers and floating point numbers.

--- ConversionLayer/ConversionLayer/extraction.h
+++ ConversionLayer/ConversionLayer/extraction.h
@@ -8,11 +8,30 @@ namespace wraplite::conversion_layer {
 	/// <summary>
 	/// Get an integer value from a column at the specified index.
 	/// </summary>
-	template<typename T> requires types::sql_integral<T>
+	template<typename T> requires types::sql_int<T>
 	T get_column(types::statement_t stmt, int column_idx) {
-
+		return sqlite3_column_type(stmt.get(), column_idx) == SQLITE_NULL ?
+			0 : static_cast<T>(sqlite3_column_int(stmt.get(), column_idx));
 	}
 
+	template<typename T> requires types::sql_bigint<T>
+	T get_column(types::statement_t stmt, int column_idx) {
+		return sqlite3_column_type(stmt.get(), column_idx) == SQLITE_NULL ?
+			0 : static_cast<T>(sqlite3_column_int64(stmt.get(), column_idx));
+	}
+
+	/// <summary>
+	/// Get a floating point value from the specified index.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="stmt"></param>
+	/// <param name="column_idx"></param>
+	/// <returns></returns>
+	template<typename T> requires types::sql_decimal<T>
+	T get_column(types::statement_t stmt, int column_idx) {
+		return sqlite3_column_type(stmt.get(), column_idx) == SQLITE_NULL ?
+			0.0 : static_cast<T>(sqlite3_column_double(stmt.get(), column_idx));
+	}
 }
 
 
