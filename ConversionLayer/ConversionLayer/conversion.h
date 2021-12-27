b5ae8e6452ba0d0cc011dd178c904f1a29f33b0a
BimmerBass <abildskovniels@gmail.com>
2021-12-27 17:10:26 +0100

    Add execution methods to conversion layer.

--- ConversionLayer/ConversionLayer/conversion.h
+++ ConversionLayer/ConversionLayer/conversion.h
@@ -1,9 +1,11 @@
 #ifndef CONVERSION_H
 #define CONVERSION_H
 #include <string>
+#include <functional>
 
 #include "types.h"
 #include "binds.h"
+#include "extraction.h"
 
 namespace wraplite::conversion_layer {
 
@@ -20,6 +22,54 @@ namespace wraplite::conversion_layer {
 	/// </summary>
 	/// <param name="session"></param>
 	void close_db(types::session_t session);
+
+	/// <summary>
+	/// Execute a query
+	/// </summary>
+	/// <typeparam name="function"></typeparam>
+	/// <param name="stmt"></param>
+	/// <param name="callback"></param>
+	void execute_query(types::statement_t stmt, std::function<void(void)> callback) {
+
+		// Keep executing until there are no more rows.
+		int result;
+		while ((result = sqlite3_step(stmt.get())) == SQLITE_ROW) {
+			callback();
+		}
+		if (result != SQLITE_DONE) { // Error!
+			throw exceptions::sqlite_exception();
+		}
+
+	}
+
+	/// <summary>
+	/// Execute a query, but only a single time.
+	/// </summary>
+	/// <typeparam name="function"></typeparam>
+	/// <param name="stmt"></param>
+	/// <param name="callback"></param>
+	void execute_single_query(types::statement_t stmt, std::function<void(void)> callback) {
+		// Execute the query once.
+		int result = sqlite3_step(stmt.get());
+
+		switch (result) {
+		case SQLITE_ROW:
+			callback(); break;
+		case SQLITE_DONE:	// There were no data, so just return.
+			return;
+		default: // An error occured.
+			throw exceptions::sqlite_exception();
+		}
+
+		// Now check if there is more data, and throw an exception if there is.
+		if (result = sqlite3_step(stmt.get()); result == SQLITE_ROW) {
+			throw std::runtime_error("the query contained multiple results. Only a single was expected.");
+		}
+
+		if (result != SQLITE_DONE) {
+			throw exceptions::sqlite_exception();
+		}
+	}
 }
 
 
